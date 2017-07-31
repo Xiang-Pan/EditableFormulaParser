27386e758f373ca56a0deda9bb1331a5372e6820
Hover <hover@hust.edu.cn>
2017-07-31 20:10:52 +0800

    PC

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,6 +17,30 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->numButton_8, SIGNAL(clicked()), this, SLOT(digitClicked()));
     connect(ui->numButton_9, SIGNAL(clicked()), this, SLOT(digitClicked()));
     connect(ui->numButton_0, SIGNAL(clicked()), this, SLOT(digitClicked()));
+    connect(ui->varButton_A, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_B, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_C, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_D, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_E, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_F, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_G, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_X, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_Y, SIGNAL(clicked()), this, SLOT(varClicked()));
+    connect(ui->varButton_Z, SIGNAL(clicked()), this, SLOT(varClicked()));
+
+
+    connect(ui->funButton_sin, SIGNAL(clicked()), this, SLOT(funClicked()));
+    connect(ui->funButton_cos, SIGNAL(clicked()), this, SLOT(funClicked()));
+    connect(ui->funButton_tan, SIGNAL(clicked()), this, SLOT(funClicked()));
+    connect(ui->funButton_arctan, SIGNAL(clicked()), this, SLOT(funClicked()));
+    /*此处进行
+       QRegExp rx("^(-?[0]|-?[1-9][0-9]{0,5})(?:\\.\\d{1,4})?$|(^\\t?$)");
+       QRegExpValidator *pReg = new QRegExpValidator(rx, this);
+       lineEdit->setValidator(pReg);
+    //设置*/
+
+
+
 }
 
 
@@ -49,35 +73,54 @@ void MainWindow::digitClicked()
     }
 }
 
-void MainWindow::funClicked()
+
+void MainWindow::varClicked()
 {
     QPushButton *clickedButton = qobject_cast<QPushButton *>(sender());
-    int digitValue = 0;
-    QString digitValue16;
-    double Pi;
-    if(clickedButton->text() == "Pi")
-    {
-        Pi = 3.1415926535;
-        ui->lineEdit_display->clear();
-    }
-    else
-    {
-            digitValue = clickedButton->text().toInt();
-    }
-
-    if (ui->lineEdit_display->text() == "0" && digitValue == 0.0)
-        return;
-
-
-    else
-    {
+    QString varChar;
+    varChar=clickedButton->text();
+    ui->lineEdit_display->setText(ui->lineEdit_display->text() + varChar);
        // if(ui->radioButton_16->isChecked())
        //     ui->lineEdit_display->setText(ui->lineEdit_display->text() + digitValue16);
        // else
-            ui->lineEdit_display->setText(ui->lineEdit_display->text() + QString::number(digitValue));
-    }
+       //ui->lineEdit_display->setText(ui->lineEdit_display->text() + QString::number(digitValue));
 }
 
+void MainWindow::funClicked()
+{
+    QPushButton *clickedButton = qobject_cast<QPushButton *>(sender());
+    QString funString;
+    funString=clickedButton->text();
+    ui->lineEdit_display->setText(ui->lineEdit_display->text() + funString+"(");
+}
+
+
+void MainWindow::saveFileSlot()
+{
+    if(saveFileName.isEmpty())//如果为空执行另存为操作
+    {
+        this->saveAsFileSlot();
+    }
+    else                      //如果不为空 则保存
+    {
+        QFile *file=new QFile;
+        file->setFileName(saveFileName);
+        bool ok=file->open(QIODevice::WriteOnly);
+        if(ok)
+        {
+            QTextStream out(file);
+            out<<ui->textEdit->toPlainText();//去除textEdit当中的纯文本 重定向到流中 放入磁盘
+            file->close();
+            this->setWindowTitle(saveFileName+"---------");
+            delete file;
+        }
+        else
+        {
+            QMessageBox::information(this,"Error Message","Save File Error");
+            return;
+        }
+    }
+}
 
 MainWindow::~MainWindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -22,6 +22,11 @@ private:
 private slots:
     void digitClicked();
     void funClicked();
+    void varClicked();
+
+    void newFileSlot();
+    void openFileSlot();//打开一个已经存在的文本文件
+    void saveFileSlot();//保存文件到磁盘
 };
 
 #endif // MAINWINDOW_H
